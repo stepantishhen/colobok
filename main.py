fc81684fdfc131e8e88acb70d4b37eb78dd054ff
Степан <stepantishhen@mail.ru>
2020-12-15 14:11:23 +0300

    Some Changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 864, 672
 TILE_SIZE = 32
@@ -44,7 +45,7 @@ class Labyrint:
             for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
                 next_x, next_y = x + dx, y + dy
                 if 0 <= next_x < self.width and 0 < next_y < self.height and \
-                    self.is_free((next_x, next_y)) and \
+                        self.is_free((next_x, next_y)) and \
                         distance[next_y][next_x] == INF:
                     distance[next_y][next_x] = distance[y][x] + 1
                     prev[next_y][next_x] = (x, y)
@@ -58,15 +59,18 @@ class Labyrint:
 
 
 class Game:
-    def __init__(self, map, colobok, enemy):
+    def __init__(self, map, colobok, enemy, coins):
         self.map = map
         self.colobok = colobok
         self.enemy = enemy
+        self.coins = coins
 
     def render(self, screen):
         self.map.render(screen)
+        self.coins.render(screen)
         self.colobok.render(screen)
         self.enemy.render(screen)
+
 
     def update_colobok(self):
         next_x, next_y = self.colobok.get_position()
@@ -109,6 +113,7 @@ class Enemy:
                     (self.x * TILE_SIZE - delta, self.y * TILE_SIZE - delta))
 
 
+
 class Colobok:
     def __init__(self, pic, position):
         self.x, self.y = position
@@ -126,16 +131,40 @@ class Colobok:
                     (self.x * TILE_SIZE - delta, self.y * TILE_SIZE - delta))
 
 
-def show_message(screen, message):
-    font = pygame.font.Font('font/elizabeta-modern.ttf', 60)
-    text = font.render(message, 1, (255, 255, 255))
-    text_x = WINDOW_WIDTH // 2 - text.get_width() // 2
-    text_y = WINDOW_HEIGHT // 2 - text.get_height() // 2
-    text_w = text.get_width()
-    text_h = text.get_height()
-    pygame.draw.rect(screen, (255, 0, 0), (text_x - 10, text_y - 10,
-                                           text_w + 20, text_h + 20))
-    screen.blit(text, (text_x, text_y))
+class FlourCoins:
+    def __init__(self, filename, free_tile, wall_tile):
+        self.map = []
+        self.rnd_coins = [(random.randint(0, 26), random.randint(0, 20)) for _ in
+               range(40)]
+        with open(f'{filename}') as input_file:
+            for line in input_file:
+                self.map.append(list(map(str, line.rstrip('\n'))))
+        self.height = len(self.map)
+        self.width = len(self.map[0])
+        self.tile_size = TILE_SIZE
+        self.free_tile = free_tile
+        self.wall_tile = wall_tile
+
+    def render(self, screen):
+        flour_coins = pygame.image.load('images/flour.png')
+        for x, y in self.rnd_coins:
+            if self.is_free((x, y)):
+                screen.blit(flour_coins, [x * TILE_SIZE, y * TILE_SIZE])
+
+    def get_tile_id(self, position):
+        return self.map[position[1]][position[0]]
+
+    def is_free(self, position):
+        return self.get_tile_id(position) in self.free_tile
+
+    def check_collision(self):
+        pass
+
+
+def show_message(screen, message, position, size):
+    font = pygame.font.Font('font/elizabeta-modern.ttf', size)
+    text = font.render(message, 1, (251, 2, 3))
+    screen.blit(text, position)
 
 
 def main_window():
@@ -144,8 +173,11 @@ def main_window():
     pygame.display.set_caption('Колобок')
     icon = pygame.image.load('images/colobok.png')
     pygame.display.set_icon(icon)
+    background_img = pygame.image.load('images/background-img.png')
+    keyboard_img = pygame.image.load('images/keyboard.png')
 
     clock = pygame.time.Clock()
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -154,7 +186,12 @@ def main_window():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 running = False
                 game_window()
-        screen.fill(pygame.Color('#BAE6FC'))
+        screen.blit(background_img, [0, 0])
+        screen.blit(keyboard_img, [0, 550])
+        show_message(screen, 'Нажмите пробел, чтобы начать играть', (50, 10),
+                     30)
+        show_message(screen, 'Управление с помощью клавиш', (180, 610),
+                     30)
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
@@ -162,7 +199,7 @@ def main_window():
 
 def game_window():
     pygame.init()
-    screen = pygame.display.set_mode(WINDOW_SIZE)
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT + 50))
     pygame.display.set_caption('Колобок')
     icon = pygame.image.load('images/colobok.png')
     pygame.display.set_icon(icon)
@@ -170,11 +207,13 @@ def game_window():
     labyrint = Labyrint("map.txt", '.', '#')
     colobok = Colobok("colobok.png", (13, 15))
     enemy = Enemy("enemy.png", (13, 9))
-    game = Game(labyrint, colobok, enemy)
+    flour = FlourCoins("map.txt", '.', '#')
+    game = Game(labyrint, colobok, enemy, flour)
 
     clock = pygame.time.Clock()
     running = True
     game_over = False
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -187,10 +226,9 @@ def game_window():
         game.render(screen)
         if game.check_lose():
             game_over = True
-            show_message(screen, 'Ты проиграл!')
+            show_message(screen, 'Съеден', (320, 300), 50)
         pygame.display.flip()
         clock.tick(FPS)
-    pygame.quit()
 
 
 if __name__ == '__main__':
